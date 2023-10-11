6d1b7114cb794fc3d11cb82670a791e5c417af28
RAFAT <muhyminul@cse.uiu.ac.bd>
2023-10-11 10:48:49 +0600

    Function is ready for feature SUM

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int sum(int x,int y){
-    
+    return x+y;
 }
 
 int main()
@@ -12,7 +12,7 @@ int main()
     cin>>a>>b;
 
     cout<<"The values that you have given input are: "<<a<<"     "<<b<<endl;
-    
+    cout<<"The sum of those two numbers are: "<<sum(a,b)<<endl;
 
     return 0;
 }
