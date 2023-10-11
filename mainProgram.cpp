e1bcc378c7fc4aaed4aff18f4663e1e92a052fc7
RAFAT <muhyminul@cse.uiu.ac.bd>
2023-10-11 10:29:21 +0600

    Input &  Priinting Functionality is added

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main()
 {
+    int a,b;
+    cout<<"Enter the input values: ";
+    cin>>a>>b;
 
+    cout<<"The values that you have given input are: "<<a<<"     "<<b<<endl;
 
     return 0;
 }
