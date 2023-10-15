ee0b3fd1b161200e9b52063cb5674fb5c03acbcc
RAFAT <muhyminul@cse.uiu.ac.bd>
2023-10-15 13:37:52 +0600

    Completed function for feature division

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+float division(int x,int y){
+    if(y!=0)
+        return float(x)/y;
+    else
+        return 0;
+}
+
 int subtract(int x,int y){
     return abs(x-y);
 }
@@ -14,6 +21,13 @@ int main()
     cout<<"The values that you have given input are: "<<a<<"     "<<b<<endl;
     
     cout<<"The subtration of those two input values are: "<<subtract(a,b)<<endl;
+
+    if(division(a,b)!=0){
+        cout<<"The division of those two input values are: "<<division(a,b)<<endl;
+    }
+    else{
+        cout<<"Divide by zero problem"<<endl;
+    }
 
     return 0;
 }
